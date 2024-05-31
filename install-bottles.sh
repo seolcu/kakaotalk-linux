2f155b09e294234f3a0e00f387a4763e2ca26d94
seolcu <seolcu0112@proton.me>
2024-05-31 09:58:16 +0900

    일단

--- install-bottles.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Set up flatpak
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-
-# Install Bottles
-flatpak install flathub com.usebottles.bottles
--- run.sh
+++ run.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
+# Set up flatpak
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
 # Install Bottles
-./install-bottles.sh
+flatpak install flathub com.usebottles.bottles
 
+# Variables
 bottles_cli="flatpak run --command=bottles-cli com.usebottles.bottles"
+bottle_name="test"
 
-$bottles_cli --help
+# Create a new bottle
+$bottles_cli new --bottle-name $bottle_name --environment application
+
+# Fonts
+wget -nc https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
+wget -nc https://github.com/googlefonts/noto-cjk/raw/main/Sans/Variable/TTF/NotoSansCJKkr-VF.ttf
+cp NotoColorEmoji.ttf NotoSansCJKkr-VF.ttf ~/.var/app/com.usebottles.bottles/data/bottles/bottles/$bottle_name/drive_c/windows/Fonts/
+
+# Run KakaoTalk Installer
+wget -nc https://app-pc.kakaocdn.net/talk/win32/under_win10/KakaoTalk_Setup.exe
+flatpak override --user --filesystem="$PWD" com.usebottles.bottles
+$bottles_cli run -b $bottle_name -e ./KakaoTalk_Setup.exe
+
+# Desktop Entry
+flatpak override com.usebottles.bottles --user --filesystem=xdg-data/applications
